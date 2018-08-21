fa1f347203d11c58bd53a777ffac44661d788524
Ksenia <karabanova.ksyusha41@gmail.com>
2018-08-21 10:29:26 +0300

    Task 2.1 Implemented ProductDao corrected

--- src/main/java/com/es/phoneshop/model/ArrayListProductDao.java
+++ src/main/java/com/es/phoneshop/model/ArrayListProductDao.java
@@ -9,46 +9,31 @@ import java.util.stream.Stream;
 
 
 public class ArrayListProductDao implements ProductDao{
-    ArrayList<Product> products;
+    private ArrayList<Product> products;
 
    public ArrayListProductDao(){
         products = new ArrayList<Product>();
-        Product pr1 = new Product(new Long(2),"12", "iPhone",new BigDecimal(10.0), 2);
-       Product pr2 = new Product(new Long(5),"42", "iPhone5",new BigDecimal(0), 1);
-       Product pr3 = new Product(new Long(7),"phone", "iPhoneX",new BigDecimal(100.0), 1);
-
-       this.save(pr1);
-       this.save(pr2);
-       this.save(pr3);
+        save(new Product(2L,"12", "iPhone",new BigDecimal(10.0), 2));
+       save( new Product(5L,"42", "iPhone5",new BigDecimal(0), 1));
+       save( new Product(7L,"phone", "iPhoneX",new BigDecimal(100.0), 1));
     }
 
 
-    public Product getProduct(Long id) {
-       synchronized (products) {
-           for (Product p : products) {
-               if (p.getId() == id) {
-                   return p;
-               }
-           }
-       }
-       return null;
+    public synchronized Product getProduct(Long id) {
+          return products.parallelStream().filter(s->s.getId() == id).findAny().get();
     }
 
-    public List<Product> findProducts() {
+    public synchronized List<Product> findProducts() {
         ArrayList<Product> result = new ArrayList<Product>();
-        synchronized (products) {
-             return products.parallelStream().filter(s->(s.getPrice().doubleValue() > 0 && s.getStock() != 0)).collect(Collectors.toList());
-        }
+             return products.parallelStream().filter(s->(s.getPrice()
+                     .doubleValue() > 0 && s.getStock() != 0))
+                     .collect(Collectors.toList());
     }
 
-    public void save(Product product) {
-       synchronized (products) {
+    public synchronized void save(Product product) {
            products.add(product);
-       }
     }
-    public void delete(Long id) {
-       synchronized (products) {
-           products.parallelStream().filter(s -> s.getId() == id).limit(1).forEach(s -> products.remove(s));
-       }
+    public synchronized void delete(Long id) {
+           products.remove(getProduct(id));
     }
 }
