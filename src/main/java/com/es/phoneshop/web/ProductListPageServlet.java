788711c1e91e0335ce39748105d46ce0c8d0f427
radiophysics <37349503+radiophysics@users.noreply.github.com>
2018-08-26 21:17:56 +0300

    Update ProductListPageServlet.java

--- src/main/java/com/es/phoneshop/web/ProductListPageServlet.java
+++ src/main/java/com/es/phoneshop/web/ProductListPageServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class ProductListServlet extends HttpServlet {
+public class ProductListPageServlet extends HttpServlet {
 
     private ProductDao productDao;
 
