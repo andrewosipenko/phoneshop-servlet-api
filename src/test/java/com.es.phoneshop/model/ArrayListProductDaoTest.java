f4b2d192ece65f674fcc378a3090433962d1391d
bewithforce <somniummisellus@gmail.com>
2018-08-27 22:32:09 +0300

    Task 2.3: View product details page

--- src/test/java/com.es.phoneshop/model/ArrayListProductDaoTest.java
+++ src/test/java/com.es.phoneshop/model/ArrayListProductDaoTest.java
@@ -12,38 +12,56 @@ public class ArrayListProductDaoTest {
     private ProductDao products = ArrayListProductDao.getInstance();
 
     @Before
-    public void init(){
-        products.save(new Product(1L,"a1", "descr1", new BigDecimal(162),
+    public void init() {
+        products.save(new Product(1L, "a1", "descr1", new BigDecimal(162),
                 Currency.getInstance("BYN"), 61));
     }
 
     @Test
-    public void getInstance(){
-        assertNotNull(ArrayListProductDao.getInstance());
+    public void testGetInstance() {
+        ProductDao productDao = ArrayListProductDao.getInstance();
+
+
+        assertNotNull(productDao);
     }
 
     @Test
-    public void getProduct() {
-        assertEquals(1L, products.getProduct(1L).getId().longValue());
+    public void testGetProduct() {
+        Long id = products.getProduct(1L).getId();
+
+
+        assertEquals(1L, id.longValue());
     }
 
     @Test
-    public void findProducts() {
-        assertFalse(products.findProducts().isEmpty());
+    public void testFindProducts() {
+        boolean isListEmpty = products.findProducts().isEmpty();
+
+
+        assertFalse(isListEmpty);
     }
 
     @Test
-    public void save() {
-        products.save(new Product(2L,"a2", "descr2", new BigDecimal(162),
+    public void testSave() {
+        products.save(new Product(2L, "a2", "descr2", new BigDecimal(162),
                 Currency.getInstance("BYN"), 61));
-        assertNotNull(products.getProduct(2L));
+
+
+        Product product = products.getProduct(2L);
+
+
+        assertNotNull(product);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void remove() {
-        products.save(new Product(4L,"a4", "descr4", new BigDecimal(123),
+    public void testRemove() {
+        products.save(new Product(4L, "a4", "descr4", new BigDecimal(123),
                 Currency.getInstance("BYN"), 16));
+
+
         products.remove(4L);
+
+
         products.getProduct(4L);
     }
 }
