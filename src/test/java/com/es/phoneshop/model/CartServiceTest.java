a167fb9bba1cf8dcbe265c625ebeb5a433b10110
Yan <jan.piatkevicz@gmail.com>
2018-09-04 19:04:58 +0300

    fix tests

--- src/test/java/com/es/phoneshop/model/CartServiceTest.java
+++ src/test/java/com/es/phoneshop/model/CartServiceTest.java
@@ -1,8 +1,6 @@
 package com.es.phoneshop.model;
 
-import com.es.phoneshop.web.ProductIDGenerator;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mockito.Mock;
 
@@ -11,37 +9,16 @@ import static org.mockito.Mockito.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import java.math.BigDecimal;
-
 import static org.junit.Assert.*;
 
 public class CartServiceTest {
     private CartService cartService = CartService.getInstance();
-    //private static ProductDao productDao;
-
-    /*@BeforeClass
-    public static void init() {
-        productDao = ArrayListProductDao.getInstance();
-        Product product;
-        for (int i = 0; i < 2; i++) {
-            product = new Product();
-            ProductIDGenerator.generateID(product);
-            product.setCode(i + "");
-            product.setDescription("description" + i);
-            product.setPrice(new BigDecimal(i));
-            product.setStock(i);
-            productDao.save(product);
-        }
-    }*/
 
     @Mock
     private HttpSession session = mock(HttpSession.class);
 
     @Mock
     private HttpServletRequest request = mock(HttpServletRequest.class);
-
-    /*@Mock
-    private ProductDao productDao = mock(ProductDao.class);*/
 
     @Before
     public void clear() {
