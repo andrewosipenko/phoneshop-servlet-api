dd0953b6dd93b2ff3cc4b69f95c247230d441d8a
ViachaslauPratasenia <slavaprot14@gmail.com>
2018-09-25 02:44:49 +0300

    Task 2.7

--- src/main/java/com/es/phoneshop/model/CartItem.java
+++ src/main/java/com/es/phoneshop/model/CartItem.java
@@ -1,43 +1,17 @@
 package com.es.phoneshop.model;
 
+import com.es.phoneshop.model.order.AbstractOrderItem;
+
 import java.io.Serializable;
 import java.util.Objects;
 
-public class CartItem implements Serializable {
+public class CartItem extends AbstractOrderItem implements Serializable {
     private Product product;
     private int quantity;
 
     public CartItem(Product product, int quantity) {
-        this.product = product;
-        this.quantity = quantity;
+        super(product, quantity);
     }
 
-    public Product getProduct() {
-        return product;
-    }
 
-    public void setProduct(Product product) {
-        this.product = product;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(product);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        CartItem cartItem = (CartItem) obj;
-        return Objects.equals(product, cartItem.product);
-    }
 }
---
+++ src/main/java/com/es/phoneshop/web/CheckoutPageServlet.java
@@ -0,0 +1,48 @@
+package com.es.phoneshop.web;
+
+import com.es.phoneshop.model.CartItem;
+import com.es.phoneshop.model.CartService;
+import com.es.phoneshop.model.CartServiceImpl;
+import com.es.phoneshop.model.order.Order;
+import com.es.phoneshop.model.order.OrderService;
+import com.es.phoneshop.model.order.OrderServiceImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CheckoutPageServlet extends HttpServlet {
+    private CartService cartService = CartServiceImpl.getInstance();
+    private OrderService orderService = OrderServiceImpl.getInstance();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.setAttribute("totalCost", sumTotalCost(request));
+        request.setAttribute("cart", cartService.getCart(request));
+        request.getRequestDispatcher("/WEB-INF/pages/checkoutPage.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Order order = orderService.placeOrder(cartService.getCart(request));
+
+        order.setName(request.getParameter("Name"));
+        order.setAddress(request.getParameter("Address"));
+        order.setPhone(request.getParameter("Phone"));
+        order.setTotalCost(sumTotalCost(request));
+
+        request.getSession().removeAttribute("cart");
+        response.sendRedirect(request.getRequestURI() + "/" + order.getId());
+    }
+
+    private int sumTotalCost(HttpServletRequest request) {
+        int totalCost = 0;
+        for(CartItem cartItem : cartService.getCart(request).getCartItems()) {
+            totalCost += cartItem.getQuantity() * cartItem.getProduct().getPrice().intValue();
+        }
+        return totalCost;
+    }
+}
