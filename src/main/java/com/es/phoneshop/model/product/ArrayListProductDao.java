ec85a0b502ed5f721ff378b7fa3b7489e18b05e2
plusqRED <rickes.oleg@gmail.com>
2019-03-14 21:19:43 +0300

    added all tests, implemented ArrayListProductDao methods, some refactoring

--- src/main/java/com/es/phoneshop/model/product/ArrayListProductDao.java
+++ src/main/java/com/es/phoneshop/model/product/ArrayListProductDao.java
@@ -1,25 +1,37 @@
 package com.es.phoneshop.model.product;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ArrayListProductDao implements ProductDao {
+
+    private List<Product> products = new ArrayList<>();
+
     @Override
     public Product getProduct(Long id) {
-        throw new RuntimeException("Not implemented");
+        if (id == null) throw new NullPointerException("Id cant be null!");
+        if (id < 0) throw new IllegalArgumentException("Id cant be negative!");
+        return products.stream().filter(p -> p.getId().equals(id)).findAny().get();
     }
 
     @Override
     public List<Product> findProducts() {
-        throw new RuntimeException("Not implemented");
+        return products;
     }
 
     @Override
     public void save(Product product) {
-        throw new RuntimeException("Not implemented");
+        if (product == null) throw new NullPointerException("Product cant be null!");
+        if (products.stream().anyMatch(p -> p.getId().equals(product.getId()))) {
+            throw new IllegalArgumentException("Product with this id already exist!");
+        }
+        products.add(product);
     }
 
     @Override
     public void delete(Long id) {
-        throw new RuntimeException("Not implemented");
+        if (id == null) throw new NullPointerException("Id cant be null!");
+        if (id < 0) throw new IllegalArgumentException("Id cant be negative!");
+        products.remove(products.stream().filter(p -> p.getId().equals(id)).findAny().get());
     }
 }
--- src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
@@ -3,16 +3,69 @@ package com.es.phoneshop.model.product;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class ArrayListProductDaoTest
-{
+public class ArrayListProductDaoTest {
     private ProductDao productDao;
 
     @Before
     public void setup() {
         productDao = new ArrayListProductDao();
     }
+
+    @Test(expected = NullPointerException.class)
+    public void testGetProductNullPointer() {
+        productDao.getProduct(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetProductIllegalArgument() {
+        productDao.getProduct(-5L);
+    }
+
+    @Test
+    public void testGetProduct() {
+        Product product = new Product();
+        product.setId(1L);
+        productDao.save(product);
+        assertEquals(product.getId(), productDao.getProduct(product.getId()).getId());
+    }
+
+
+    @Test(expected = NullPointerException.class)
+    public void testDeleteProductNullPointer() {
+        productDao.delete(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testDeleteProductIllegalArgument() {
+        productDao.delete(-5L);
+    }
+
+
+    @Test(expected = NullPointerException.class)
+    public void testSaveProductNullPointer() {
+        productDao.save(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSaveProductIllegalArgument() {
+        Product product1 = new Product();
+        product1.setId(1L);
+        Product product2 = new Product();
+        product2.setId(1L); // setting up same id
+        productDao.save(product1);
+        productDao.save(product2);
+    }
+
+    @Test
+    public void testSaveProduct() {
+        Product product = new Product();
+        productDao.save(product);
+        assertEquals(1, productDao.findProducts().size());
+    }
+
 
     @Test
     public void testFindProductsNoResults() {
