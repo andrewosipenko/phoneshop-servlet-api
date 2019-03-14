36fa4552a47361826c954ea971f39c2d8714f53c
Yegor-Ikbaev <yegor.ikbaev@gmail.com>
2019-03-14 14:35:55 +0300

    Task 1.5. Implemented ProductDao

--- src/main/java/com/es/phoneshop/model/product/ArrayListProductDao.java
+++ src/main/java/com/es/phoneshop/model/product/ArrayListProductDao.java
@@ -1,25 +1,54 @@
 package com.es.phoneshop.model.product;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ArrayListProductDao implements ProductDao {
-    @Override
-    public Product getProduct(Long id) {
-        throw new RuntimeException("Not implemented");
-    }
 
-    @Override
-    public List<Product> findProducts() {
-        throw new RuntimeException("Not implemented");
-    }
+	private List<Product> products;
 
-    @Override
-    public void save(Product product) {
-        throw new RuntimeException("Not implemented");
-    }
+	public ArrayListProductDao() {
+		products = new ArrayList<>();
+	}
 
-    @Override
-    public void delete(Long id) {
-        throw new RuntimeException("Not implemented");
-    }
+	@Override
+	public synchronized Product getProduct(Long id) throws IllegalArgumentException {
+		return products.parallelStream()
+				       .filter((product) -> product.getId().equals(id))
+				       .findAny()
+				       .orElseThrow(() -> new IllegalArgumentException("There is no product with such id"));
+	}
+
+	@Override
+	public synchronized List<Product> findProducts() {
+		return products.stream().filter((product) -> {
+			boolean isValid = false;
+			if (product.getPrice() != null && product.getStock() > 0) {
+				isValid = true;
+			}
+			return isValid;
+		}).collect(Collectors.toList());
+	}
+
+	@Override
+	public synchronized void save(Product product) throws IllegalArgumentException {
+		Product productWithSameId;
+		try {
+			productWithSameId = getProduct(product.getId());
+		} catch (IllegalArgumentException exception) {
+			productWithSameId = null;
+		}
+		
+		if (productWithSameId == null) {
+			products.add(product);
+		} else {
+			throw new IllegalArgumentException("Product with such id is already exists");
+		}
+	}
+
+	@Override
+	public synchronized void delete(Long id) throws IllegalArgumentException {
+		products.remove(getProduct(id));
+	}
 }
--- src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
@@ -3,19 +3,86 @@ package com.es.phoneshop.model.product;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class ArrayListProductDaoTest
-{
-    private ProductDao productDao;
+import java.math.BigDecimal;
 
-    @Before
-    public void setup() {
-        productDao = new ArrayListProductDao();
-    }
+public class ArrayListProductDaoTest {
+	private ProductDao productDao;
 
-    @Test
-    public void testFindProductsNoResults() {
-        assertTrue(productDao.findProducts().isEmpty());
-    }
+	@Before
+	public void setup() {
+		productDao = new ArrayListProductDao();
+	}
+
+	@Test
+	public void testFindProductsNoResults() {
+		assertTrue(productDao.findProducts().isEmpty());
+	}
+
+	@Test
+	public void testFindProductAfterSaving() {
+		Product product = new Product();
+		product.setPrice(new BigDecimal(1));
+		product.setStock(1);
+		productDao.save(product);
+		assertEquals(1, productDao.findProducts().size());
+	}
+
+	@Test
+	public void testFindProductAfterSavingWithStockLess0() {
+		Product product = new Product();
+		product.setPrice(new BigDecimal(1));
+		product.setStock(0);
+		productDao.save(product);
+		assertEquals(0, productDao.findProducts().size());
+	}
+
+	@Test
+	public void testFindProductAfterSavingNoResults() {
+		Product product = new Product();
+		productDao.save(product);
+		assertEquals(0, productDao.findProducts().size());
+	}
+
+	@Test
+	public void testGetProductById() {
+		Product product = new Product();
+		Long id = 1L;
+		product.setId(id);
+		productDao.save(product);
+		assertEquals(id, productDao.getProduct(id).getId());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testGetProductByIdNoResult() {
+		productDao.getProduct(1L);
+	}
+
+	@Test
+	public void testDeleteProduct() {
+		Product product = new Product();
+		Long id = 1L;
+		product.setId(id);
+		productDao.save(product);
+		productDao.delete(id);
+		assertEquals(0, productDao.findProducts().size());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testDeleteProductNoSuchProduct() {
+		productDao.delete(1L);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testSaveProductsWithSameId() {
+		Long id = 1L;
+		Product product1 = new Product();
+		Product product2 = new Product();
+		product1.setId(id);
+		product2.setId(id);
+		productDao.save(product1);
+		productDao.save(product2);
+	}
 }
