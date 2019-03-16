88f7383382b4392ef5c10ef1d95c55f9f1e4c3b4
ivbir <ivan.biruchov@gmail.com>
2019-03-16 14:53:34 +0300

    Task 1.5 Implemented ProductDao

--- src/main/java/com/es/phoneshop/model/product/ArrayListProductDao.java
+++ src/main/java/com/es/phoneshop/model/product/ArrayListProductDao.java
@@ -1,25 +1,41 @@
 package com.es.phoneshop.model.product;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 public class ArrayListProductDao implements ProductDao {
+    private List<Product> products = new ArrayList<>();
+
     @Override
-    public Product getProduct(Long id) {
-        throw new RuntimeException("Not implemented");
+    public Product getProduct(Long id) throws NoSuchElementException {
+        return products.stream().
+                filter(product -> product.getId().equals(id)).
+                findAny().
+                orElseThrow(() -> new NoSuchElementException(id.toString()));
     }
 
     @Override
-    public List<Product> findProducts() {
-        throw new RuntimeException("Not implemented");
+    synchronized public List<Product> findProducts() {
+        return products.stream().
+                filter(product -> null != product.getPrice()).
+                filter(product -> product.getStock() > 0).
+                collect(Collectors.toList());
     }
 
     @Override
     public void save(Product product) {
-        throw new RuntimeException("Not implemented");
+        products.add(product);
     }
 
     @Override
-    public void delete(Long id) {
-        throw new RuntimeException("Not implemented");
+    public void delete(Long id) throws NoSuchElementException {
+        products.remove(
+                products.stream().
+                        filter(product -> product.getId().equals(id)).
+                        findAny().
+                        orElseThrow(() -> new NoSuchElementException(id.toString()))
+        );
     }
 }
