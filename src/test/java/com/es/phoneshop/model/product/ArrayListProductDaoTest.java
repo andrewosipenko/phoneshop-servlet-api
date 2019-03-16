dd818fcf81bbcee04b72f053fa030e00158aa8f9
ivbir <ivan.biruchov@gmail.com>
2019-03-16 15:07:49 +0300

    Implement ArrayListProductDaoTest

--- src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
@@ -3,10 +3,16 @@ package com.es.phoneshop.model.product;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
 
-public class ArrayListProductDaoTest
-{
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ArrayListProductDaoTest {
     private ProductDao productDao;
 
     @Before
@@ -16,6 +22,113 @@ public class ArrayListProductDaoTest
 
     @Test
     public void testFindProductsNoResults() {
+        assertTrue(productDao.findProducts().isEmpty());
+    }
+
+    @Test
+    public void testSuccessfulExecutionSaveAndFindProducts() {
+        Product product = mock(Product.class);
+        BigDecimal price = new BigDecimal(1);
+        Integer stock = 1;
+
+        when(product.getPrice()).thenReturn(price);
+        when(product.getStock()).thenReturn(stock);
+
+        productDao.save(product);
+
+        List<Product> expected = new ArrayList<>();
+        expected.add(product);
+        List<Product> actual = productDao.findProducts();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFindProductsNotInStock() {
+        Product product = mock(Product.class);
+        BigDecimal price = new BigDecimal(1);
+        Integer stock = 0;
+
+        when(product.getPrice()).thenReturn(price);
+        when(product.getStock()).thenReturn(stock);
+
+        productDao.save(product);
+
+        assertTrue(productDao.findProducts().isEmpty());
+    }
+
+    @Test
+    public void testFindProductsNullPrice() {
+        Product product = mock(Product.class);
+        BigDecimal price = null;
+        Integer stock = 1;
+
+        when(product.getPrice()).thenReturn(price);
+        when(product.getStock()).thenReturn(stock);
+
+        productDao.save(product);
+
+        assertTrue(productDao.findProducts().isEmpty());
+    }
+
+    @Test
+    public void testGetProductNoResult() {
+        Long realId = 1L;
+        Long failId = 0L;
+        Product product = mock(Product.class);
+
+        when(product.getId()).thenReturn(realId);
+        productDao.save(product);
+
+        try {
+            productDao.getProduct(failId);
+            fail("Expected NoSuchElementException.");
+        } catch (NoSuchElementException expected) {
+            assertEquals(expected.getMessage(), failId.toString());
+        }
+    }
+
+    @Test
+    public void testSuccessfulExecutionGetProduct() {
+        Long id = 1L;
+        Product product = mock(Product.class);
+
+        when(product.getId()).thenReturn(id);
+        productDao.save(product);
+
+        Product expected = product;
+        Product actual = productDao.getProduct(id);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testDeleteProductNoResult() {
+        Long realId = 1L;
+        Long failId = 0L;
+        Product product = mock(Product.class);
+
+        when(product.getId()).thenReturn(realId);
+        productDao.save(product);
+
+        try {
+            productDao.delete(failId);
+            fail("Expected NoSuchElementException.");
+        } catch (NoSuchElementException expected) {
+            assertEquals(expected.getMessage(), failId.toString());
+        }
+    }
+
+    @Test
+    public void testSuccessfulExecutionDeleteProduct() {
+        Long id = 1L;
+        Product product = mock(Product.class);
+
+        when(product.getId()).thenReturn(id);
+
+        productDao.save(product);
+        productDao.delete(id);
+
         assertTrue(productDao.findProducts().isEmpty());
     }
 }
