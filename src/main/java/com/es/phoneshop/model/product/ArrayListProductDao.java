51868d0e00113c3726cdec48e47c715e3889acc8
AlPsvch <pasevich.aleksej@gmail.com>
2019-03-17 15:44:04 +0300

    Task 1.5 Implemented ProductDao

--- src/main/java/com/es/phoneshop/model/product/ArrayListProductDao.java
+++ src/main/java/com/es/phoneshop/model/product/ArrayListProductDao.java
@@ -1,25 +1,43 @@
 package com.es.phoneshop.model.product;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ArrayListProductDao implements ProductDao {
+    private List<Product> products = new ArrayList<>();
+
     @Override
-    public Product getProduct(Long id) {
-        throw new RuntimeException("Not implemented");
+    public synchronized Product getProduct(Long id) {
+        return products.stream()
+                .filter((p) -> p.getId().equals(id) && p.getPrice() != null && p.getStock() > 0)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("There is no product with such ID."));
     }
 
     @Override
-    public List<Product> findProducts() {
-        throw new RuntimeException("Not implemented");
+    public synchronized List<Product> findProducts() {
+        return products.stream()
+                .filter((p) -> p.getPrice() != null && p.getStock() > 0)
+                .collect(Collectors.toList());
     }
 
     @Override
-    public void save(Product product) {
-        throw new RuntimeException("Not implemented");
+    public synchronized void save(Product product) {
+        if (products.stream()
+                .anyMatch((p) -> p.getId().equals(product.getId()))) {
+            throw new IllegalArgumentException("Product with such ID already exists");
+        } else {
+            products.add(product);
+        }
     }
 
     @Override
-    public void delete(Long id) {
-        throw new RuntimeException("Not implemented");
+    public synchronized void delete(Long id) {
+        products.stream()
+                .filter((p) -> p.getId().equals(id))
+                .findFirst()
+                .map((p) -> products.remove(p))
+                .orElseThrow(() -> new IllegalArgumentException("There is no product with such ID."));
     }
 }
