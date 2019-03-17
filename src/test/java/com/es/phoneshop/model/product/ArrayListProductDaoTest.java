8dcecb43ea3409a79a539495d71bce520ce57703
AlPsvch <pasevich.aleksej@gmail.com>
2019-03-17 15:46:23 +0300

    Task 1.5 Described ProductDaoTest

--- src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
@@ -2,11 +2,15 @@ package com.es.phoneshop.model.product;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 
+import java.math.BigDecimal;
+import java.util.Currency;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class ArrayListProductDaoTest
-{
+public class ArrayListProductDaoTest {
     private ProductDao productDao;
 
     @Before
@@ -14,8 +18,29 @@ public class ArrayListProductDaoTest
         productDao = new ArrayListProductDao();
     }
 
+    @Mock
+    Currency usd;
+
+    @Mock
+    String imageURL;
+
     @Test
     public void testFindProductsNoResults() {
         assertTrue(productDao.findProducts().isEmpty());
+    }
+
+    @Test
+    public void testSaveAndDelete() {
+        Product product = new Product(1L, "sgs", "Samsung Galaxy S", new BigDecimal(100), usd, 100, imageURL);
+
+        productDao.save(product);
+        assertEquals(1, productDao.findProducts().size());
+        productDao.delete(1L);
+        assertEquals(0, productDao.findProducts().size());
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testGetProduct(){
+        productDao.getProduct(1L);
     }
 }
