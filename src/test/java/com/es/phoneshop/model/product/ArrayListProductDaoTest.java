0d9a7743330736f4b1e75b1d0f542ce108def9de
Pasha <pashakirvel9@gmail.com>
2019-07-12 09:39:17 +0300

    homework

--- src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
@@ -2,20 +2,67 @@ package com.es.phoneshop.model.product;
 
 import org.junit.Before;
 import org.junit.Test;
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.Optional;
+import static junit.framework.TestCase.assertEquals;
 
-import static org.junit.Assert.assertTrue;
+
 
 public class ArrayListProductDaoTest
 {
     private ProductDao productDao;
+    private Product product;
+    private Product productSave;
+    private Currency usd ;
 
     @Before
     public void setup() {
         productDao = new ArrayListProductDao();
+        usd = Currency.getInstance("USD");
+        product = new Product(3L, "sgs3", "Samsung Galaxy S III", new BigDecimal(300), usd, 5, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S%20III.jpg");
+        productSave = new Product(14L, "sgs", "Samsung Galaxy S", new BigDecimal(100), usd, 100, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg");
+
     }
 
     @Test
-    public void testFindProductsNoResults() {
-        assertTrue(productDao.findProducts().isEmpty());
+    public void testFindProducts() {
+        int sizeBefore = productDao.findProducts().size();
+        Product product = new Product(3L, "sgs3", "Samsung Galaxy S III", null, usd, 5, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S%20III.jpg");
+        productDao.save(product);
+        assertEquals(sizeBefore,productDao.findProducts().size());
     }
+
+    @Test
+    public void  testFindProductById(){
+        Long id = 3L;
+        assertEquals(product,productDao.getProduct(id));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testFindProductByIdException(){
+        Long id = -1L;
+        productDao.getProduct(id);
+    }
+
+
+    @Test
+    public void testDeleteProduct(){
+        int sizeBefore = productDao.findProducts().size();
+        Long id = 3L;
+        productDao.delete(id);
+        assertEquals(sizeBefore - 1,productDao.findProducts().size());
+
+    }
+
+    @Test
+    public void testSaveProduct(){
+        int sizeBefore = productDao.findProducts().size();
+        productDao.save(productSave);
+        assertEquals(sizeBefore + 1,productDao.findProducts().size());
+
+    }
+
+
+
 }
