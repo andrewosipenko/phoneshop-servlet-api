301d3ec3bc1f44788c6022773fac08ed757f9ad0
Dmitry Chervonenko <dima1772000@gmail.com>
2019-07-13 21:34:44 +0300

    Task 1.5 Implemented ProductDao

--- src/main/java/com/es/phoneshop/model/product/ArrayListProductDao.java
+++ src/main/java/com/es/phoneshop/model/product/ArrayListProductDao.java
@@ -1,25 +1,55 @@
 package com.es.phoneshop.model.product;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ArrayListProductDao implements ProductDao {
+    private static List<Product> products = new ArrayList<>();
     @Override
-    public Product getProduct(Long id) {
-        throw new RuntimeException("Not implemented");
+    public synchronized Product getProduct(Long id) {
+        Product product;
+        int index = products.indexOf(new Product(id));
+        if (index!=-1){
+            product = products.get(index);
+
+        } else{
+            return null;
+        }
+        if (product!=null){
+            return product;
+        } else {
+            return null;
+        }
     }
 
     @Override
-    public List<Product> findProducts() {
-        throw new RuntimeException("Not implemented");
+    public synchronized List<Product> findProducts() {
+        List<Product> filtered = new ArrayList<>();
+        products.stream().filter(prod->prod.getPrice()!=null && prod.getStock()>0).forEach(prod->filtered.add(prod));
+        return filtered;
     }
 
     @Override
-    public void save(Product product) {
-        throw new RuntimeException("Not implemented");
+    public synchronized void save(Product product) {
+        if (products.indexOf(product) == -1){
+            products.add(product);
+        } else{
+            products.set(products.indexOf(product),product);
+        }
     }
 
     @Override
-    public void delete(Long id) {
-        throw new RuntimeException("Not implemented");
+    public synchronized void delete(Long id) {
+        int ind=-1;
+        for (Product prd: products) {
+            if(prd.getId()==id){
+                ind = products.indexOf(prd);
+                break;
+            }
+        }
+        if(ind!=-1){
+            products.remove(ind);
+        }
     }
+
 }
