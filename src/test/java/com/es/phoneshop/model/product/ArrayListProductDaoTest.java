29dd2d702ff3118af2ba14af6b0c2ec1e3ab5124
Dmitry Chervonenko <dima1772000@gmail.com>
2019-07-13 21:46:31 +0300

    Tests

--- src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
@@ -3,7 +3,11 @@ package com.es.phoneshop.model.product;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 public class ArrayListProductDaoTest
 {
@@ -17,5 +21,49 @@ public class ArrayListProductDaoTest
     @Test
     public void testFindProductsNoResults() {
         assertTrue(productDao.findProducts().isEmpty());
+    }
+
+    @Test
+    public void testGetProduct(){
+        Product testProduct = new Product(35L);
+        productDao.save(testProduct);
+        Product prd = productDao.getProduct(35L);
+        assertEquals(testProduct,prd);
+    }
+
+    @Test
+    public void testGetProductWrong(){
+        Product testProduct = new Product(10L);
+        productDao.save(testProduct);
+        Product wrongProduct = productDao.getProduct(15L);
+        assertNotEquals(testProduct,wrongProduct);
+
+    }
+
+    @Test
+    public void testSave(){
+        Product product = new Product(56L);
+        productDao.save(product);
+        assertEquals(product,productDao.getProduct(product.getId()));
+    }
+
+    @Test
+    public void testSaveDuplicate(){
+        Product product1 = new Product(1L, "sgs", "Samsung Galaxy S", new BigDecimal(100), Currency.getInstance("USD"), 100, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg");
+        Product product2 =new Product(1L, "sgs2", "Samsung Galaxy S II", new BigDecimal(200), Currency.getInstance("USD"), 10, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S%20II.jpg");
+        productDao.save(product1);
+        productDao.save(product2);
+        List<Product> products = productDao.findProducts();
+        assertEquals(products.indexOf(product1),products.lastIndexOf(product2));
+    }
+
+    @Test
+    public void testDelete(){
+        boolean before, after;
+        productDao.save(new Product(10L));
+        before = (productDao.getProduct(10L)!=null);
+        productDao.delete(10L);
+        after = productDao.getProduct(10L) == null;
+        assertTrue(before && after);
     }
 }
