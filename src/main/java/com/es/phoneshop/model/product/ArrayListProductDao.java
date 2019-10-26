fff1dca62f2ca147b2e343de9b309c199defbb2d
Vika <vika.zhak1999@gmail.com>
2019-10-26 18:50:54 +0300

    Initial implementation of ArrayListProductDao methods

--- src/main/java/com/es/phoneshop/model/product/ArrayListProductDao.java
+++ src/main/java/com/es/phoneshop/model/product/ArrayListProductDao.java
@@ -1,25 +1,43 @@
 package com.es.phoneshop.model.product;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ArrayListProductDao implements ProductDao {
+    List<Product> products;
+
+    public ArrayListProductDao() {
+        products = new ArrayList<>();
+    }
+
+
     @Override
     public Product getProduct(Long id) {
-        throw new RuntimeException("Not implemented");
+        return products.stream().
+                filter(product -> product.getId().equals(id)).
+                findAny().orElseThrow( () -> new RuntimeException( "Product is not found"));
     }
 
     @Override
     public List<Product> findProducts() {
-        throw new RuntimeException("Not implemented");
+        return products.stream().
+                filter(product -> product.getPrice() != null && product.getStock() > 0).
+                collect(Collectors.toList());
     }
 
     @Override
     public void save(Product product) {
-        throw new RuntimeException("Not implemented");
+        if (product == null) {
+            throw new IllegalArgumentException("Product can not be null");
+        }
+        else {
+            products.add(product);
+        }
     }
 
     @Override
     public void delete(Long id) {
-        throw new RuntimeException("Not implemented");
+        products.remove(getProduct(id));
     }
 }
