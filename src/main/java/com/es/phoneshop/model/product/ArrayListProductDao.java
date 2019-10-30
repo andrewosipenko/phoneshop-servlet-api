6ed197ebac4f58690ce84ca5f0d36b16dd1f4d1a
Vika <vika.zhak1999@gmail.com>
2019-10-30 10:09:27 +0300

    fixed method save and reformatted streams

--- src/main/java/com/es/phoneshop/model/product/ArrayListProductDao.java
+++ src/main/java/com/es/phoneshop/model/product/ArrayListProductDao.java
@@ -1,7 +1,5 @@
 package com.es.phoneshop.model.product;
-
 import com.es.phoneshop.model.exception.ProductNotFoundException;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,33 +13,29 @@ public class ArrayListProductDao implements ProductDao {
 
     @Override
     public synchronized Product getProduct(Long id) {
-        return products.stream().
-                filter(product -> product.getId().equals(id)).
-                findAny().orElseThrow( () -> new ProductNotFoundException( "Product is not found"));
+        return products.stream()
+                .filter(product -> product.getId().equals(id))
+                .findAny()
+                .orElseThrow( () -> new ProductNotFoundException( "Product is not found"));
     }
 
     @Override
     public synchronized List<Product> findProducts() {
-        return products.stream().
-                filter(product -> product.getPrice() != null && product.getStock() > 0).
-                collect(Collectors.toList());
+        return products.stream()
+                .filter(product -> product.getPrice() != null && product.getStock() > 0)
+                .collect(Collectors.toList());
     }
 
     @Override
     public synchronized void save(Product product) {
-        Product productWithSameId;
-        if (product == null || product.getId() == null) {
-            throw new IllegalArgumentException("Product or id can not be null");
-        }
-        try {
-            productWithSameId = getProduct(product.getId());
-        } catch (ProductNotFoundException exception) {
-            productWithSameId = null;
-        }
-        if (productWithSameId == null) {
-            products.add(product);
-        } else {
+        if (products.contains(product)){
             throw new IllegalArgumentException("Product with such id is already exists");
+        }
+        if (product != null || product.getId() != null){
+            products.add(product);
+        }
+        else {
+            throw new IllegalArgumentException("Product or id can not be null");
         }
     }
 
