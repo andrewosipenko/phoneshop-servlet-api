5868989fb44439ff59a63bf7ca7370e5b3a78f08
Ilya Papino <papino2000@outlook.com>
2020-02-02 17:28:44 +0300

    Task 1.5 Implemented ProductDao

--- src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
@@ -3,10 +3,13 @@ package com.es.phoneshop.model.product;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+import java.util.Currency;
+
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class ArrayListProductDaoTest
-{
+public class ArrayListProductDaoTest {
     private ProductDao productDao;
 
     @Before
@@ -16,6 +19,28 @@ public class ArrayListProductDaoTest
 
     @Test
     public void testFindProductsNoResults() {
-        assertTrue(productDao.findProducts().isEmpty());
+        assertTrue(!productDao.findProducts().isEmpty());
     }
+
+    @Test
+    public void testNullPrice() {
+        assertFalse(productDao.findProducts().stream()
+                .filter(product -> product.getPrice() == null)
+                .findAny()
+                .isPresent()
+        );
+    }
+
+    @Test
+    public void testGetProduct() {
+        assertTrue(productDao.getProduct(4L) != null);
+    }
+
+    @Test
+    public void testSaveProduct() {
+        Product prod = new Product(14L, "nokia111", "Nokia 355", new BigDecimal(50), Currency.getInstance("USD"), 10, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg");
+        productDao.save(prod);
+        assertTrue(productDao.getProduct(prod.getId()) == prod);
+    }
+
 }
