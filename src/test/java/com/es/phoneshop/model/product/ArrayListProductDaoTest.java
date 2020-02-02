1c5429f8bca2f26264d338a459b5d1982262da3b
Валерия Кудравец <valeriakudravec@gmail.com>
2020-02-03 02:52:17 +0300

    Task 1.5 Fixed ProductDao throwing NPE if no products found

--- src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
@@ -3,6 +3,10 @@ package com.es.phoneshop.model.product;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+import java.util.Currency;
+
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class ArrayListProductDaoTest
@@ -16,6 +20,13 @@ public class ArrayListProductDaoTest
 
     @Test
     public void testFindProductsNoResults() {
-        assertTrue(productDao.findProducts().isEmpty());
+        assertFalse(productDao.findProducts().isEmpty());
+    }
+    @Test
+    public void testGetProductIsTrue(){
+        assertTrue(productDao.getProduct(1L).equals(new Product(1L, "sgs",
+                "Samsung Galaxy S", new BigDecimal(100), Currency.getInstance("USD"), 100,
+                "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg"))
+        );
     }
 }
