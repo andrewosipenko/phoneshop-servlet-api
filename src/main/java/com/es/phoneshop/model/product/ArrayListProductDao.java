598e405003a1dbe5f5e8a29c0f83283a20c92a9c
DeniskaRediskaa <denis.usevich@tut.by>
2021-01-28 12:06:31 +0000

    Task 1.5 Implemented ProductDao

--- src/main/java/com/es/phoneshop/model/product/ArrayListProductDao.java
+++ src/main/java/com/es/phoneshop/model/product/ArrayListProductDao.java
@@ -1,25 +1,106 @@
 package com.es.phoneshop.model.product;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Currency;
 import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.stream.Collectors;
 
 public class ArrayListProductDao implements ProductDao {
+
+    private final List<Product> products = new ArrayList<>();
+    private final ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+    private final Lock readLock = readWriteLock.readLock();
+    private final Lock writeLock = readWriteLock.writeLock();
+    private long maxId;
+
+    public ArrayListProductDao() {
+        initProducts();
+    }
+
+    public long getMaxId() {
+        return maxId;
+    }
+
     @Override
-    public Product getProduct(Long id) {
-        throw new RuntimeException("Not implemented");
+    public Product getProduct(Long id) throws ProductNotFoundException {
+        if (id == null) throw new ProductNotFoundException();
+        readLock.lock();
+        try {
+            return products.stream()
+                    .filter(product -> id.equals(product.getId()))
+                    .findAny()
+                    .orElseThrow(ProductNotFoundException::new);
+        } finally {
+            readLock.unlock();
+        }
     }
 
     @Override
     public List<Product> findProducts() {
-        throw new RuntimeException("Not implemented");
+        readLock.lock();
+        try {
+            return products.stream()
+                    .filter(product -> product.getStock() > 0)
+                    .filter(product -> product.getPrice() != null)
+                    .collect(Collectors.toList());
+        } finally {
+            readLock.unlock();
+        }
     }
 
     @Override
     public void save(Product product) {
-        throw new RuntimeException("Not implemented");
+        if (product != null) {
+            writeLock.lock();
+            if (product.getId() != null) {
+                products.stream()
+                        .filter(p -> product.getId().equals(p.getId()))
+                        .findAny()
+                        .ifPresentOrElse(val -> products.set(products.indexOf(val), product),
+                                () -> products.add(product));
+            } else {
+                try {
+                    product.setId(maxId++);
+                    products.add(product);
+                } finally {
+                    writeLock.unlock();
+                }
+            }
+        }
     }
 
     @Override
     public void delete(Long id) {
-        throw new RuntimeException("Not implemented");
+        if (id != null) {
+            writeLock.lock();
+            try {
+                products.removeIf(product -> id.equals(product.getId()));
+            } finally {
+                writeLock.unlock();
+            }
+        }
+    }
+
+    private void initProducts() {
+        writeLock.lock();
+        Currency usd = Currency.getInstance("USD");
+        save(new Product("sgs", "Samsung Galaxy S", new BigDecimal(100), usd, 100, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg"));
+        save(new Product("sgs2", "Samsung Galaxy S II", new BigDecimal(200), usd, 0, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S%20II.jpg"));
+        save(new Product("sgs3", "Samsung Galaxy S III", new BigDecimal(300), usd, 5, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S%20III.jpg"));
+        save(new Product("iphone", "Apple iPhone", new BigDecimal(200), usd, 10, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Apple/Apple%20iPhone.jpg"));
+        save(new Product("iphone6", "Apple iPhone 6", new BigDecimal(1000), usd, 30, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Apple/Apple%20iPhone%206.jpg"));
+        save(new Product("htces4g", "HTC EVO Shift 4G", new BigDecimal(320), usd, 3, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/HTC/HTC%20EVO%20Shift%204G.jpg"));
+        save(new Product("sec901", "Sony Ericsson C901", new BigDecimal(420), usd, 30, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Sony/Sony%20Ericsson%20C901.jpg"));
+        save(new Product("xperiaxz", "Sony Xperia XZ", new BigDecimal(120), usd, 100, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Sony/Sony%20Xperia%20XZ.jpg"));
+        save(new Product("nokia3310", "Nokia 3310", new BigDecimal(70), usd, 100, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Nokia/Nokia%203310.jpg"));
+        save(new Product("palmp", "Palm Pixi", new BigDecimal(170), usd, 30, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Palm/Palm%20Pixi.jpg"));
+        save(new Product("simc56", "Siemens C56", new BigDecimal(70), usd, 20, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Siemens/Siemens%20C56.jpg"));
+        save(new Product("simc61", "Siemens C61", new BigDecimal(80), usd, 30, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Siemens/Siemens%20C61.jpg"));
+        save(new Product("simsxg75", "Siemens SXG75", new BigDecimal(150), usd, 40, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Siemens/Siemens%20SXG75.jpg"));
+        writeLock.unlock();
     }
 }
--- src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
@@ -3,19 +3,69 @@ package com.es.phoneshop.model.product;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import java.math.BigDecimal;
+import java.util.Currency;
 
-public class ArrayListProductDaoTest
-{
+import static org.junit.Assert.*;
+
+public class ArrayListProductDaoTest {
     private ProductDao productDao;
+    private Currency usd;
 
     @Before
     public void setup() {
         productDao = new ArrayListProductDao();
+        usd = Currency.getInstance("USD");
     }
 
     @Test
-    public void testFindProductsNoResults() {
-        assertTrue(productDao.findProducts().isEmpty());
+    public void testFindProducts() {
+        assertFalse(productDao.findProducts().isEmpty());
     }
+
+    @Test
+    public void testSaveProduct() throws ProductNotFoundException {
+        Product product = new Product("sgs", "Samsung Galaxy S", new BigDecimal(100), usd, 100, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg");
+        productDao.save(product);
+        assertNotNull(product.getId());
+        assertEquals(product, productDao.getProduct(product.getId()));
+    }
+
+    @Test
+    public void testUpdateProduct() throws ProductNotFoundException {
+        productDao.save((new Product(0L, "updated", "Samsung Galaxy S", new BigDecimal(100), usd, 100, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg")));
+        Product product = productDao.getProduct(0L);
+        assertEquals("updated", product.getCode());
+    }
+
+    @Test
+    public void testSaveProductWithExistingUniqueId() throws ProductNotFoundException {
+        long id = ((ArrayListProductDao) productDao).getMaxId() + 5L;
+        Product product = new Product(id, "updated", "Samsung Galaxy S", new BigDecimal(100), usd, 100, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg");
+        productDao.save(product);
+        assertEquals(product,productDao.getProduct(id));
+    }
+
+    @Test
+    public void testSuccessfulDeleteProduct() {
+        int sizeBefore = productDao.findProducts().size();
+        productDao.delete(2L);
+        int sizeAfter = productDao.findProducts().size();
+        assertEquals(sizeBefore, sizeAfter + 1);
+    }
+
+    @Test
+    public void testUnsuccessfulDeleteProduct() {
+        int sizeBefore = productDao.findProducts().size();
+        productDao.delete(-5L);
+        int sizeAfter = productDao.findProducts().size();
+        assertEquals(sizeBefore, sizeAfter);
+    }
+
+    @Test
+    public void testGetProduct() throws ProductNotFoundException {
+        Product product = productDao.getProduct(2L);
+        assertNotNull(product);
+    }
+
 }
