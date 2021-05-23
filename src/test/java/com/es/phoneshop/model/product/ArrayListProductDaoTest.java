335b4df93c80072164aede4da00c437919e84d8d
VadimMityai <vadim.mityai@gmail.com>
2021-05-23 21:37:08 +0300

    Task 1.5 and 1.6

--- src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
@@ -3,7 +3,11 @@ package com.es.phoneshop.model.product;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.NoSuchElementException;
+
+import static org.junit.Assert.*;
 
 public class ArrayListProductDaoTest
 {
@@ -12,10 +16,29 @@ public class ArrayListProductDaoTest
     @Before
     public void setup() {
         productDao = new ArrayListProductDao();
+       }
+
+    @Test
+    public void testFindProductsResults() {
+        assertFalse(productDao.findProducts().isEmpty());
     }
 
     @Test
-    public void testFindProductsNoResults() {
-        assertTrue(productDao.findProducts().isEmpty());
+    public void testSaveUpdateNewProduct() {
+        Currency usd = Currency.getInstance("USD");
+        Product product = new Product("db", "Samsung Galaxy S", new BigDecimal(100), usd, 100, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg");
+        Product product1 = new Product(1L,"abs", "Samsung Galaxy S", new BigDecimal(100), usd, 100, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg");
+        productDao.save(product);
+        productDao.save(product1);
+        Product result = productDao.getProduct(product.getId());
+        assertEquals("db", result.getCode());
+        result = productDao.getProduct(product1.getId());
+        assertEquals("abs", result.getCode());
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testRemoveProduct() {
+        productDao.delete(1L);
+        productDao.getProduct(1L);
     }
 }
