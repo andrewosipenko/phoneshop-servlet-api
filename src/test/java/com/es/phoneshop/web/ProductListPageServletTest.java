ef32b70893777dcb406fc7228c450c501a426417
Alex Yakubovskiy <iamavtoritet24.11@yandex.ru>
2021-05-24 11:22:02 +0300

    Added Servlet test

--- src/test/java/com/es/phoneshop/web/ProductListPageServletTest.java
+++ src/test/java/com/es/phoneshop/web/ProductListPageServletTest.java
@@ -1,5 +1,6 @@
 package com.es.phoneshop.web;
 
+import lombok.SneakyThrows;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,30 +13,33 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ProductListPageServletTest {
-    @Mock
-    private HttpServletRequest request;
-    @Mock
-    private HttpServletResponse response;
-    @Mock
-    private RequestDispatcher requestDispatcher;
+	@Mock
+	private HttpServletRequest request;
+	@Mock
+	private HttpServletResponse response;
+	@Mock
+	private RequestDispatcher requestDispatcher;
 
-    private ProductListPageServlet servlet = new ProductListPageServlet();
+	private ProductListPageServlet servlet = new ProductListPageServlet();
 
-    @Before
-    public void setup(){
-        when(request.getRequestDispatcher(anyString())).thenReturn(requestDispatcher);
-    }
+	@SneakyThrows
+	@Before
+	public void setup() {
+		when(request.getRequestDispatcher(anyString())).thenReturn(requestDispatcher);
+	}
 
-    @Test
-    public void testDoGet() throws ServletException, IOException {
-        servlet.doGet(request, response);
+	@Test
+	public void testDoGet() throws ServletException, IOException {
+		servlet.doGet(request, response);
+		verify(requestDispatcher).forward(request, response);
+		verify(request).setAttribute(eq("products"), any());
+	}
 
-        verify(requestDispatcher).forward(request, response);
-    }
+
 }
