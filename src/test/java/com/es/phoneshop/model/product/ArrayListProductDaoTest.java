59c001f56f97bb7995e87429022c357fdd94b71a
Alex <iamavtoritet24.11@yandex.ru>
2021-06-01 22:50:28 +0300

    Fixed tests with method findProducts (added param)

--- src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
@@ -10,76 +10,80 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class ArrayListProductDaoTest {
-	private ProductDao productDao;
+    private ProductDao productDao;
 
-	@Before
-	public void setup() {
-		productDao = ArrayListProductDao.getInstance();
-	}
+    @Before
+    public void setup() {
+        productDao = ArrayListProductDao.getInstance();
+    }
 
-	@Test
-	public void testSaveProduct() {
-		Currency usd = Currency.getInstance("USD");
-		Product product = new Product("simsxg75", "Siemens SXG75", new BigDecimal(150), usd, 40, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Siemens/Siemens%20SXG75.jpg");
-		productDao.save(product);
+    @Test
+    public void testSaveProduct() {
+        Currency usd = Currency.getInstance("USD");
+        Product product = new Product("simsxg75", "Siemens SXG75", new BigDecimal(150), usd, 40, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Siemens/Siemens%20SXG75.jpg");
+        productDao.save(product);
 
-		assertTrue(productDao.findProducts().contains(product));
-	}
+        assertTrue(productDao.findProducts("", SortField.description, SortOrder.asc).contains(product));
+    }
 
-	@Test
-	public void testSaveProductWithId() {
-		Currency usd = Currency.getInstance("USD");
-		Product product = new Product("simsxg75", "Siemens SXG75", new BigDecimal(150), usd, 40, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Siemens/Siemens%20SXG75.jpg");
-		productDao.save(product);
+    @Test
+    public void testSaveProductWithId() {
+        Currency usd = Currency.getInstance("USD");
+        Product product = new Product("nok3310", "Nokia3310", new BigDecimal(150), usd, 40, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Siemens/Siemens%20SXG75.jpg");
+        productDao.save(product);
 
-		final long id = product.getId();
-		Product updProduct = new Product(id, "siemens75", "Siemens SXG75", new BigDecimal(200), usd, 40, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Siemens/Siemens%20SXG75.jpg");
-		productDao.save(updProduct);
+        final long id = product.getId();
+        Product updProduct = new Product(id, "siemens75", "Siemens SXG75", new BigDecimal(200), usd, 40, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Siemens/Siemens%20SXG75.jpg");
+        productDao.save(updProduct);
 
-		assertFalse(productDao.findProducts().contains(product));
-		assertTrue(productDao.findProducts().contains(updProduct));
-	}
+        assertFalse(productDao.findProducts("nokia", SortField.description, SortOrder.asc).contains(product));
+        assertTrue(productDao.findProducts("siemens", SortField.description, SortOrder.asc).contains(updProduct));
+    }
 
-	@Test
-	public void testGetProduct() {
-		Currency usd = Currency.getInstance("USD");
-		Product product = new Product("simsxg75", "Siemens SXG75", new BigDecimal(150), usd, 40, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Siemens/Siemens%20SXG75.jpg");
-		productDao.save(product);
+    @Test
+    public void testGetProduct() {
+        Currency usd = Currency.getInstance("USD");
+        Product product = new Product("simsxg75", "Siemens SXG75", new BigDecimal(150), usd, 40, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Siemens/Siemens%20SXG75.jpg");
+        productDao.save(product);
 
-		final long id = product.getId();
-		assertTrue(productDao.getProduct(id).isPresent());
-	}
+        final long id = product.getId();
+        assertTrue(productDao.getProduct(id).isPresent());
+    }
 
-	@Test
-	public void testDeleteAndGetProductNull() {
-		Currency usd = Currency.getInstance("USD");
-		Product product = new Product("simsxg75", "Siemens SXG75", new BigDecimal(150), usd, 40, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Siemens/Siemens%20SXG75.jpg");
-		productDao.save(product);
-		final long id = product.getId();
-		assertTrue(productDao.getProduct(id).isPresent());
+    @Test
+    public void testDeleteAndGetProductNull() {
+        Currency usd = Currency.getInstance("USD");
+        Product product = new Product("simsxg75", "Siemens SXG75", new BigDecimal(150), usd, 40, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Siemens/Siemens%20SXG75.jpg");
+        productDao.save(product);
+        final long id = product.getId();
+        assertTrue(productDao.getProduct(id).isPresent());
 
-		productDao.delete(id);
-		assertFalse(productDao.getProduct(id).isPresent());
-	}
+        productDao.delete(id);
+        assertFalse(productDao.getProduct(id).isPresent());
+    }
 
-	@Test
-	public void testFindProducts() {
-		Currency usd = Currency.getInstance("USD");
-		Product product = new Product("simsxg75", "Siemens SXG75", new BigDecimal(150), usd, 40, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Siemens/Siemens%20SXG75.jpg");
-		productDao.save(product);
+    @Test
+    public void testFindProducts() {
+        Currency usd = Currency.getInstance("USD");
+        Product product = new Product("simsxg75", "Siemens SXG75", new BigDecimal(150), usd, 40, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Siemens/Siemens%20SXG75.jpg");
+        productDao.save(product);
 
-		assertFalse(productDao.findProducts().isEmpty());
-		assertTrue(productDao.findProducts().contains(product));
-	}
+        assertTrue(productDao.findProducts("siemens", SortField.description, SortOrder.asc).contains(product));
+    }
 
-	@Test
-	public void testFindProductsZeroStock() {
-		Currency usd = Currency.getInstance("USD");
-		Product product = new Product("simsxg75", "Siemens SXG75", new BigDecimal(150), usd, 0, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Siemens/Siemens%20SXG75.jpg");
-		productDao.save(product);
-		final long id = product.getId();
+    @Test
+    public void testFindProductsNoResultsWithThisSearchQuery() {
+        assertTrue(productDao.findProducts("nokia", SortField.description, SortOrder.asc).isEmpty());
+    }
 
-		assertTrue(productDao.getProduct(id).isPresent());
-		assertFalse(productDao.findProducts().contains(product));
-	}
+    @Test
+    public void testFindProductsZeroStock() {
+        Currency usd = Currency.getInstance("USD");
+        Product product = new Product("simsxg75", "Siemens SXG75", new BigDecimal(150), usd, 0, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Siemens/Siemens%20SXG75.jpg");
+        productDao.save(product);
+        final long id = product.getId();
+
+        assertTrue(productDao.getProduct(id).isPresent());
+        assertFalse(productDao.findProducts("siemens", SortField.description, SortOrder.asc).contains(product));
+    }
 }
