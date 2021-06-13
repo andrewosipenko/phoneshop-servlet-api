fd2528827d87dae3d0d615188cc9bad43a77bb7a
Alex <iamavtoritet24.11@yandex.ru>
2021-06-13 21:34:58 +0300

    fixed cart service (Added volatile & fixed add method)

--- src/main/java/com/es/phoneshop/model/cart/DefaultCartService.java
+++ src/main/java/com/es/phoneshop/model/cart/DefaultCartService.java
@@ -5,14 +5,16 @@ import com.es.phoneshop.dao.ProductDao;
 import com.es.phoneshop.exception.OutOfStockException;
 import com.es.phoneshop.exception.ProductNotFoundException;
 import com.es.phoneshop.model.product.Product;
+import lombok.NonNull;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
 
 public class DefaultCartService implements CartService {
     private static final String CART_SESSION_ATTRIBUTE = DefaultCartService.class.getName() + ".cart";
 
     private ProductDao productDao;
-    private static DefaultCartService instance;
+    public static volatile DefaultCartService instance;
 
     private DefaultCartService() {
         productDao = ArrayListProductDao.getInstance();
@@ -30,7 +32,7 @@ public class DefaultCartService implements CartService {
     }
 
     @Override
-    public synchronized Cart getCart(HttpServletRequest request) {
+    public synchronized Cart getCart(@NonNull final HttpServletRequest request) {
         Cart cart = (Cart) request.getSession().getAttribute(CART_SESSION_ATTRIBUTE);
         if (cart == null) {
             request.getSession().setAttribute(CART_SESSION_ATTRIBUTE, cart = new Cart());
@@ -39,19 +41,34 @@ public class DefaultCartService implements CartService {
     }
 
     @Override
-    public synchronized void add(Cart cart, Long productId, int quantity) throws OutOfStockException {
-        Product product = productDao.getProduct(productId)
-                .orElseThrow(() -> new ProductNotFoundException(productId));
-        if (product.getStock() > quantity) {
-            throw new OutOfStockException(product, quantity, product.getStock());
+    public synchronized void add(@NonNull final Cart cart, @NonNull final Long productId, final int quantity) throws OutOfStockException {
+        if (productId < 0) {
+            throw new IllegalArgumentException("Incorrect product id");
+        }
+        if (quantity < 1) {
+            throw new IllegalArgumentException("Incorrect quantity of products");
         }
 
-        if (cart.getItems().contains(product)) {
+        Product product = productDao.getProduct(productId)
+                .orElseThrow(() -> new ProductNotFoundException(productId));
+
+        Optional<CartItem> cartItem = cart.getItems().stream()
+                .filter(item -> item.getProduct().getId().equals(product.getId()))
+                .findAny();
+
+        final int currentQuantity;
+        currentQuantity = cartItem.map(CartItem::getQuantity).orElse(0);
+
+        if (product.getStock() < quantity + currentQuantity) {
+            throw new OutOfStockException(product, quantity + currentQuantity, product.getStock());
+        }
+
+        if (cartItem.isPresent()) {
             cart.getItems().forEach(item -> {
-                        if (item.getProduct() == product) {
-                            item.setQuantity(item.getQuantity() + quantity);
-                        }
-                    });
+                if (item.getProduct().getId().equals(product.getId())) {
+                    item.setQuantity(currentQuantity + quantity);
+                }
+            });
         } else {
             cart.getItems().add(new CartItem(product, quantity));
         }
