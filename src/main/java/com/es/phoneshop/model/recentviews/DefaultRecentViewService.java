c87320c361c6b3c43e9216fd8921e866b6038631
Alex <iamavtoritet24.11@yandex.ru>
2021-06-13 21:39:58 +0300

    fixed recent view service

--- src/main/java/com/es/phoneshop/model/recentviews/DefaultRecentViewService.java
+++ src/main/java/com/es/phoneshop/model/recentviews/DefaultRecentViewService.java
@@ -1,12 +1,15 @@
 package com.es.phoneshop.model.recentviews;
 
 import com.es.phoneshop.model.product.Product;
+import lombok.NonNull;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
 
 public class DefaultRecentViewService implements RecentViewService {
     private static final String RECENT_VIEW_SESSION_ATTRIBUTE = DefaultRecentViewService.class.getName() + ".recent_views";
-    private static DefaultRecentViewService instance;
+    private static volatile DefaultRecentViewService instance;
+    private static final int NUMBER_OF_PRODUCTS = 3;
 
     private DefaultRecentViewService() {
 
@@ -24,7 +27,7 @@ public class DefaultRecentViewService implements RecentViewService {
     }
 
     @Override
-    public RecentView getRecentView(HttpServletRequest request) {
+    public RecentView getRecentView(@NonNull final HttpServletRequest request) {
         RecentView recentView = (RecentView) request.getSession().getAttribute(RECENT_VIEW_SESSION_ATTRIBUTE);
         if (recentView == null) {
             request.getSession().setAttribute(RECENT_VIEW_SESSION_ATTRIBUTE, recentView = new RecentView());
@@ -33,13 +36,14 @@ public class DefaultRecentViewService implements RecentViewService {
     }
 
     @Override
-    public void add(RecentView recentView, Product product) {
-        if (recentView.getRecentlyViewed().contains(product)) return;
-
-        if (recentView.getRecentlyViewed().size() == 3) {
-            recentView.getRecentlyViewed().remove(0);
-        }
+    public void add(@NonNull final RecentView recentView,@NonNull final Product product) {
+        recentView.getRecentlyViewed().remove(product);
 
         recentView.getRecentlyViewed().add(product);
+        Collections.rotate(recentView.getRecentlyViewed(), 1);
+
+        if (recentView.getRecentlyViewed().size() == NUMBER_OF_PRODUCTS + 1) {
+            recentView.getRecentlyViewed().remove(NUMBER_OF_PRODUCTS);
+        }
     }
 }
