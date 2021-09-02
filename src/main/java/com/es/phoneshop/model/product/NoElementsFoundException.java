827ac2cbf000c6a763aa02cbba8eaedc289335b2
Axinalis <antontrus@gmail.com>
2021-09-02 13:51:54 +0300

    Task 1.5 Implement ProductDao

---
+++ src/main/java/com/es/phoneshop/model/product/NoElementsFoundException.java
@@ -0,0 +1,13 @@
+package com.es.phoneshop.model.product;
+
+public class NoElementsFoundException extends Exception {
+
+	public NoElementsFoundException(String message) {
+		super(message);
+	}
+
+	public NoElementsFoundException() {
+
+	}
+
+}
