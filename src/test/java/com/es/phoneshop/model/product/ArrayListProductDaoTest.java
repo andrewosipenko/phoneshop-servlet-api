bb2503b06fbd665f57f9913370248887fa4dcbd2
Axinalis <antontrus@gmail.com>
2021-09-02 19:41:45 +0300

    Tests for ArrayListProductDao

--- src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
@@ -1,21 +1,64 @@
 package com.es.phoneshop.model.product;
 
+import java.util.Currency;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+
+import java.math.BigDecimal;
 
 public class ArrayListProductDaoTest
 {
     private ProductDao productDao;
+	private Product newProduct;
 
     @Before
     public void setup() {
         productDao = new ArrayListProductDao();
+		newProduct = new Product(17L, "htces4g", "HTC UVO Short 4G", new BigDecimal(32),
+				Currency.getInstance("USD"), 3, "HTC/HTC%20EVO%20Shift%204G.jpg");
     }
 
     @Test
     public void testFindProductsNoResults() {
-        assertTrue(productDao.findProducts().isEmpty());
+        assertFalse(productDao.findProducts().isEmpty());
     }
+    
+    @Test
+	public void testSavingAndGettingProduct() throws NoElementsFoundException, IdIsNotValidException, ProductIsNullException {
+		productDao.save(newProduct);
+		
+		assertEquals(newProduct, productDao.getProduct(newProduct.getId()));
+		assertNotNull(productDao.getProduct(newProduct.getId()));
+	}
+
+	@Test
+	public void testGettingProductAfterDeleting() {
+		
+	}
+
+	@Test
+	public void testSavingDifferentProductsWithOneId() throws NoElementsFoundException, IdIsNotValidException, ProductIsNullException {
+		Product oldProduct = new Product(17L, "htces4g", "HTC UVO Short 5G", new BigDecimal(42),
+				Currency.getInstance("USD"), 8, "HTC/HTC%20EVO%20Shift%204G.jpg");
+		productDao.save(oldProduct);
+		productDao.save(newProduct);
+		
+		assertEquals(productDao.getProduct(17L), newProduct);
+		assertNotEquals(productDao.getProduct(17L),oldProduct);
+	}
+
+	@Test
+	public void testAreRequestedProductsSatisfyRequerments() {
+		
+		assertTrue(productDao.findProducts()
+				.stream()
+				.allMatch(p -> (p.getStock()>0 && p.getPrice() != null)));
+	}
 }
--- src/test/java/com/es/phoneshop/web/ProductListPageServletTest.java
+++ src/test/java/com/es/phoneshop/web/ProductListPageServletTest.java
@@ -7,6 +7,7 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -24,12 +25,15 @@ public class ProductListPageServletTest {
     private HttpServletResponse response;
     @Mock
     private RequestDispatcher requestDispatcher;
+    @Mock
+    private ServletConfig config;
 
     private ProductListPageServlet servlet = new ProductListPageServlet();
 
     @Before
-    public void setup(){
+    public void setup() throws ServletException{
         when(request.getRequestDispatcher(anyString())).thenReturn(requestDispatcher);
+        servlet.init(config);
     }
 
     @Test
