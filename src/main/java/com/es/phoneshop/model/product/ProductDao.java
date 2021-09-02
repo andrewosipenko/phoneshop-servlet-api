54944fbcb5f507ad468be620b263e0d2bdf9bcf0
anton <anton@anton-hplaptop15bs0xx>
2021-09-02 13:45:18 +0300

    Task 1.5 Implement ProductDao

--- src/main/java/com/es/phoneshop/model/product/ProductDao.java
+++ src/main/java/com/es/phoneshop/model/product/ProductDao.java
@@ -3,8 +3,8 @@ package com.es.phoneshop.model.product;
 import java.util.List;
 
 public interface ProductDao {
-    Product getProduct(Long id);
+    Product getProduct(Long id) throws NoElementsFoundException, IdIsNotValidException;
     List<Product> findProducts();
-    void save(Product product);
-    void delete(Long id);
+    void save(Product product) throws ProductIsNullException;
+    void delete(Long id) throws IdIsNotValidException;
 }
--- src/test/java/com/es/phoneshop/web/ProductListPageServletTest.java
+++ src/test/java/com/es/phoneshop/web/ProductListPageServletTest.java
@@ -7,6 +7,7 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,18 +19,22 @@ import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ProductListPageServletTest {
+	
     @Mock
     private HttpServletRequest request;
     @Mock
     private HttpServletResponse response;
     @Mock
     private RequestDispatcher requestDispatcher;
+    @Mock
+    private ServletConfig config;
 
     private ProductListPageServlet servlet = new ProductListPageServlet();
 
     @Before
-    public void setup(){
+    public void setup() throws ServletException{
         when(request.getRequestDispatcher(anyString())).thenReturn(requestDispatcher);
+        servlet.init(config);
     }
 
     @Test
@@ -37,5 +42,6 @@ public class ProductListPageServletTest {
         servlet.doGet(request, response);
 
         verify(requestDispatcher).forward(request, response);
+        
     }
 }
