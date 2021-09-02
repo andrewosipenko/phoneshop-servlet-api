5d163a1a3a89b74884bd7ccad9dab151e07f14e8
Axinalis <antontrus@gmail.com>
2021-09-02 18:55:18 +0300

    Revert "Task 1.5 Implement ProductDao"
    This reverts commit 54944fbcb5f507ad468be620b263e0d2bdf9bcf0.

--- src/main/java/com/es/phoneshop/model/product/ArrayListProductDao.java
+++ src/main/java/com/es/phoneshop/model/product/ArrayListProductDao.java
@@ -1,112 +1,25 @@
 package com.es.phoneshop.model.product;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Currency;
 import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.stream.Collectors;
 
 public class ArrayListProductDao implements ProductDao {
+    @Override
+    public Product getProduct(Long id) {
+        throw new RuntimeException("Not implemented");
+    }
 
-	private List<Product> products;
-	private final Object lock = new Object();
+    @Override
+    public List<Product> findProducts() {
+        throw new RuntimeException("Not implemented");
+    }
 
-	public ArrayListProductDao() {
-		products = getSampleProducts();
-	}
-	
-	@Override
-	public Product getProduct(Long id) throws NoElementsFoundException, IdIsNotValidException {
+    @Override
+    public void save(Product product) {
+        throw new RuntimeException("Not implemented");
+    }
 
-		synchronized(lock){
-			if (id == null) {
-				throw new IdIsNotValidException("Id is null");
-			}
-
-			try {
-				return products.stream()
-						.filter(p -> id.equals(p.getId()))
-						.findAny()
-						.get();
-			} catch (NoSuchElementException ex) {
-				throw new NoElementsFoundException("No elements with current id were found");
-			}
-		}
-	}
-
-	@Override
-	public List<Product> findProducts() {
-		
-		synchronized(lock) {
-			return products.stream()
-					.filter(p -> p.getStock() > 0)
-					.filter(p -> p.getPrice() != null)
-					.collect(Collectors.toList());
-		}
-	}
-
-	@Override
-	public void save(Product product) throws ProductIsNullException {
-		
-		if(product == null) {
-			throw new ProductIsNullException("Product is null");
-		}
-		
-		List<Product> sameIdProducts;
-		
-		synchronized(lock) {
-			sameIdProducts = products.stream()
-					.filter(p -> product.getId().equals(p.getId()))
-					.collect(Collectors.toList());
-			
-			if(sameIdProducts.size()<1) {
-				products.add(product);
-			} else {
-				products.set(products.indexOf(sameIdProducts.get(0)), product);
-				
-			}
-		}
-	}
-
-	@Override
-	public void delete(Long id) throws IdIsNotValidException {
-		
-		if(id == null) {
-			throw new IdIsNotValidException("Id is null");
-		}
-		
-		synchronized(lock) {
-			try {
-				products.remove(products.stream()
-						.filter(p -> id.equals(p.getId()))
-						.findFirst()
-						.get());
-			}catch(NoSuchElementException ex) {
-				throw new IdIsNotValidException("No elements with current id were found");
-			}
-		}
-	}
-
-	private List<Product> getSampleProducts() {
-		
-		List<Product> result = new ArrayList<Product>();
-		Currency usd = Currency.getInstance("USD");
-		
-		result.add(new Product(1L, "sgs", "Samsung Galaxy S", 			new BigDecimal(100), usd, 100, "Samsung/Samsung%20Galaxy%20S.jpg"));
-		result.add(new Product(2L, "sgs2", "Samsung Galaxy S II", 		new BigDecimal(200), usd, 0, "Samsung/Samsung%20Galaxy%20S%20II.jpg"));
-		result.add(new Product(3L, "sgs3", "Samsung Galaxy S III", 		new BigDecimal(300), usd, 5, "Samsung/Samsung%20Galaxy%20S%20III.jpg"));
-		result.add(new Product(4L, "iphone", "Apple iPhone",			new BigDecimal(200), usd, 10, "Apple/Apple%20iPhone.jpg"));
-		result.add(new Product(5L, "iphone6", "Apple iPhone 6", 		new BigDecimal(1000), usd, 30, "Apple/Apple%20iPhone%206.jpg"));
-		result.add(new Product(6L, "htces4g", "HTC EVO Shift 4G", 		new BigDecimal(320), usd, 3, "HTC/HTC%20EVO%20Shift%204G.jpg"));
-		result.add(new Product(7L, "sec901", "Sony Ericsson C901", 		new BigDecimal(420), usd, 30, "Sony/Sony%20Ericsson%20C901.jpg"));
-		result.add(new Product(8L, "xperiaxz", "Sony Xperia XZ",		new BigDecimal(120), usd, 100, "Sony/Sony%20Xperia%20XZ.jpg"));
-		result.add(new Product(9L, "nokia3310", "Nokia 3310", 			new BigDecimal(70), usd, 100, "Nokia/Nokia%203310.jpg"));
-		result.add(new Product(10L, "palmp", "Palm Pixi", 				new BigDecimal(170), usd, 30, "Palm/Palm%20Pixi.jpg"));
-		result.add(new Product(11L, "simc56", "Siemens C56", 			new BigDecimal(70), usd, 20, "Siemens/Siemens%20C56.jpg"));
-		result.add(new Product(12L, "simc61", "Siemens C61", 			new BigDecimal(80), usd, 30, "Siemens/Siemens%20C61.jpg"));
-		result.add(new Product(13L, "simsxg75", "Siemens SXG75", 		new BigDecimal(150), usd, 40, "Siemens/Siemens%20SXG75.jpg"));
-
-		return result;
-	}
+    @Override
+    public void delete(Long id) {
+        throw new RuntimeException("Not implemented");
+    }
 }
--- src/test/java/com/es/phoneshop/web/ProductListPageServletTest.java
+++ src/test/java/com/es/phoneshop/web/ProductListPageServletTest.java
@@ -7,7 +7,6 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,22 +18,18 @@ import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ProductListPageServletTest {
-	
     @Mock
     private HttpServletRequest request;
     @Mock
     private HttpServletResponse response;
     @Mock
     private RequestDispatcher requestDispatcher;
-    @Mock
-    private ServletConfig config;
 
     private ProductListPageServlet servlet = new ProductListPageServlet();
 
     @Before
-    public void setup() throws ServletException{
+    public void setup(){
         when(request.getRequestDispatcher(anyString())).thenReturn(requestDispatcher);
-        servlet.init(config);
     }
 
     @Test
@@ -42,6 +37,5 @@ public class ProductListPageServletTest {
         servlet.doGet(request, response);
 
         verify(requestDispatcher).forward(request, response);
-        
     }
 }
