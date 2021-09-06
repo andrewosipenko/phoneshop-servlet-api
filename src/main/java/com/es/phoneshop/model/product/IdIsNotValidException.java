12e711cd46cc06882368d04587dd533cc238fe73
Axinalis <antontrus@gmail.com>
2021-09-06 15:28:13 +0300

    Added forgotten exception classes

---
+++ src/main/java/com/es/phoneshop/model/product/IdIsNotValidException.java
@@ -0,0 +1,12 @@
+package com.es.phoneshop.model.product;
+
+public class IdIsNotValidException extends Exception {
+
+	public IdIsNotValidException(String message) {
+		super(message);
+	}
+
+	public IdIsNotValidException() {
+	}
+
+}
---
+++ src/main/java/com/es/phoneshop/model/product/ProductIsNullException.java
@@ -0,0 +1,13 @@
+package com.es.phoneshop.model.product;
+
+public class ProductIsNullException extends Exception {
+
+	public ProductIsNullException(String message) {
+		super(message);
+	}
+
+	public ProductIsNullException() {
+
+	}
+
+}
