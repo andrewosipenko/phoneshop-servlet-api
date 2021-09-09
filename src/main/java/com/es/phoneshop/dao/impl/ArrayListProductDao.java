e8f91b91ed8cccbf48aa163ea309d902c408625f
Olya <olyakorolko74@gmail.com>
2021-09-10 00:57:05 +0300

    new fixes

--- src/main/java/com/es/phoneshop/dao/impl/ArrayListProductDao.java
+++ src/main/java/com/es/phoneshop/dao/impl/ArrayListProductDao.java
@@ -41,50 +41,61 @@ public class ArrayListProductDao implements ProductDao {
     @Override
     public List<Product> findProducts() {
         readWriteLock.readLock().lock();
-        List<Product> foundProducts = products.stream()
-                .filter(product -> product.getPrice() != null && product.getStock() > 0)
-                .collect(Collectors.toList());
-        readWriteLock.readLock().unlock();
-        return foundProducts;
+        try {
+            return products.stream()
+                    .filter(product -> product.getPrice() != null && product.getStock() > 0)
+                    .collect(Collectors.toList());
+        } finally {
+            readWriteLock.readLock().unlock();
+        }
     }
 
     @Override
     public void save(Product product) {
         readWriteLock.writeLock().lock();
-        Optional<Product> oldProduct = products.stream().filter(pr -> pr.getId().equals(product.getId())).findAny();
-        if (oldProduct.isPresent()) {
-            int index = products.indexOf(oldProduct.get());
-            products.set(index, product);
-        } else {
-            product.setId(maxId++);
-            products.add(product);
+        try {
+            Optional<Product> oldProduct = products.stream().filter(pr -> pr.getId().equals(product.getId())).findAny();
+            if (oldProduct.isPresent()) {
+                int index = products.indexOf(oldProduct.get());
+                products.set(index, product);
+            } else {
+                product.setId(maxId++);
+                products.add(product);
+            }
+        } finally {
+            readWriteLock.writeLock().unlock();
         }
-        readWriteLock.writeLock().unlock();
     }
 
     @Override
     public void delete(Long id) {
         readWriteLock.writeLock().lock();
-        products.removeIf(product -> product.getId().equals(id));
-        readWriteLock.writeLock().unlock();
+        try {
+            products.removeIf(product -> product.getId().equals(id));
+        } finally {
+            readWriteLock.writeLock().unlock();
+        }
     }
 
     private void getSampleProducts() {
         readWriteLock.writeLock().lock();
-        Currency usd = Currency.getInstance("USD");
-        save(new Product("sgs", "Samsung Galaxy S", new BigDecimal(100), usd, 100, "/Samsung/Samsung%20Galaxy%20S.jpg"));
-        save(new Product("sgs2", "Samsung Galaxy S II", new BigDecimal(200), usd, 0, "/Samsung/Samsung%20Galaxy%20S%20II.jpg"));
-        save(new Product("sgs3", "Samsung Galaxy S III", new BigDecimal(300), usd, 5, "/Samsung/Samsung%20Galaxy%20S%20III.jpg"));
-        save(new Product("iphone", "Apple iPhone", new BigDecimal(200), usd, 10, "/Apple/Apple%20iPhone.jpg"));
-        save(new Product("iphone6", "Apple iPhone 6", new BigDecimal(1000), usd, 30, "/Apple/Apple%20iPhone%206.jpg"));
-        save(new Product("htces4g", "HTC EVO Shift 4G", new BigDecimal(320), usd, 3, "/HTC/HTC%20EVO%20Shift%204G.jpg"));
-        save(new Product("sec901", "Sony Ericsson C901", new BigDecimal(420), usd, 30, "/Sony/Sony%20Ericsson%20C901.jpg"));
-        save(new Product("xperiaxz", "Sony Xperia XZ", new BigDecimal(120), usd, 100, "/Sony/Sony%20Xperia%20XZ.jpg"));
-        save(new Product("nokia3310", "Nokia 3310", new BigDecimal(70), usd, 100, "/Nokia/Nokia%203310.jpg"));
-        save(new Product("palmp", "Palm Pixi", new BigDecimal(170), usd, 30, "/Palm/Palm%20Pixi.jpg"));
-        save(new Product("simc56", "Siemens C56", new BigDecimal(70), usd, 20, "/Siemens/Siemens%20C56.jpg"));
-        save(new Product("simc61", "Siemens C61", new BigDecimal(80), usd, 30, "/Siemens/Siemens%20C61.jpg"));
-        save(new Product("simsxg75", "Siemens SXG75", new BigDecimal(150), usd, 40, "/Siemens/Siemens%20SXG75.jpg"));
-        readWriteLock.writeLock().unlock();
+        try {
+            Currency usd = Currency.getInstance("USD");
+            save(new Product("sgs", "Samsung Galaxy S", new BigDecimal(100), usd, 100, "/Samsung/Samsung%20Galaxy%20S.jpg"));
+            save(new Product("sgs2", "Samsung Galaxy S II", new BigDecimal(200), usd, 0, "/Samsung/Samsung%20Galaxy%20S%20II.jpg"));
+            save(new Product("sgs3", "Samsung Galaxy S III", new BigDecimal(300), usd, 5, "/Samsung/Samsung%20Galaxy%20S%20III.jpg"));
+            save(new Product("iphone", "Apple iPhone", new BigDecimal(200), usd, 10, "/Apple/Apple%20iPhone.jpg"));
+            save(new Product("iphone6", "Apple iPhone 6", new BigDecimal(1000), usd, 30, "/Apple/Apple%20iPhone%206.jpg"));
+            save(new Product("htces4g", "HTC EVO Shift 4G", new BigDecimal(320), usd, 3, "/HTC/HTC%20EVO%20Shift%204G.jpg"));
+            save(new Product("sec901", "Sony Ericsson C901", new BigDecimal(420), usd, 30, "/Sony/Sony%20Ericsson%20C901.jpg"));
+            save(new Product("xperiaxz", "Sony Xperia XZ", new BigDecimal(120), usd, 100, "/Sony/Sony%20Xperia%20XZ.jpg"));
+            save(new Product("nokia3310", "Nokia 3310", new BigDecimal(70), usd, 100, "/Nokia/Nokia%203310.jpg"));
+            save(new Product("palmp", "Palm Pixi", new BigDecimal(170), usd, 30, "/Palm/Palm%20Pixi.jpg"));
+            save(new Product("simc56", "Siemens C56", new BigDecimal(70), usd, 20, "/Siemens/Siemens%20C56.jpg"));
+            save(new Product("simc61", "Siemens C61", new BigDecimal(80), usd, 30, "/Siemens/Siemens%20C61.jpg"));
+            save(new Product("simsxg75", "Siemens SXG75", new BigDecimal(150), usd, 40, "/Siemens/Siemens%20SXG75.jpg"));
+        } finally {
+            readWriteLock.writeLock().unlock();
+        }
     }
 }
