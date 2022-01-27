22ac831052a222b52dc9e99ffc0493d1b572720e
Дмитрий <dimam200312@tut.by>
2022-01-27 15:14:22 +0300

    Task 1.5 Implement ProductDao

--- src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
@@ -3,7 +3,10 @@ package com.es.phoneshop.model.product;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import java.math.BigDecimal;
+import java.util.Currency;
+
+import static org.junit.Assert.*;
 
 public class ArrayListProductDaoTest
 {
@@ -15,7 +18,46 @@ public class ArrayListProductDaoTest
     }
 
     @Test
-    public void testFindProductsNoResults() {
-        assertTrue(productDao.findProducts().isEmpty());
+    public void testDeleteProduct() {
+        Currency usd = Currency.getInstance("USD");
+        Product p = new Product("sgs", "Xiaomi", new BigDecimal(100), usd, 100, "https://just/a/shortened/link.jpg");
+        productDao.save(p);
+        assertTrue(productDao.findProducts().contains(p));
+        productDao.delete(p.getId());
+        assertFalse(productDao.findProducts().contains(p));
+    }
+
+    @Test
+    public void testFindProducts() {
+        assertFalse(productDao.findProducts().isEmpty());
+        Currency usd = Currency.getInstance("USD");
+        Product p = new Product("sgs", "Xiaomi", new BigDecimal(100), usd, 100, "https://just/a/shortened/link.jpg");
+        productDao.save(p);
+        assertTrue(productDao.findProducts().contains(p));
+        p = new Product("sgs", "Xiaomi", new BigDecimal(100), usd, 0, "https://just/a/shortened/link.jpg");
+        productDao.save(p);
+        assertFalse(productDao.findProducts().contains(p));
+        p = new Product("sgs", "Xiaomi", null, usd, 10, "https://just/a/shortened/link.jpg");
+        productDao.save(p);
+        assertFalse(productDao.findProducts().contains(p));
+    }
+
+    @Test
+    public void testSaveProduct() {
+        Currency usd = Currency.getInstance("USD");
+        Product p = new Product("sgs", "Samsung Galaxy S", new BigDecimal(100), usd, 100, "https://just/a/shortened/link.jpg");
+        productDao.save(p);
+        Long id = p.getId();
+        assertNotNull(id);
+        assertEquals(productDao.getProduct(p.getId()), p);
+        productDao.save(p);
+        assertEquals(p.getId(), id);
+    }
+    @Test
+    public void testGetProduct() {
+        Currency usd = Currency.getInstance("USD");
+        Product p = new Product("sgs", "Samsung Galaxy S", new BigDecimal(100), usd, 100, "https://just/a/shortened/link.jpg");
+        productDao.save(p);
+        assertEquals(productDao.getProduct(p.getId()), p);
     }
 }
