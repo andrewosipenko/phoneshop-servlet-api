38b81dfb3ad0c2c8db548883500a54ee8c5f348c
sidorovichyan <sidorovich.yan@bk.ru>
2022-12-30 22:06:32 +0300

    Task 1.5: Implement ProductDao

---
+++ src/main/java/com/es/phoneshop/dao/impl/ArrayListProductDao.java
@@ -0,0 +1,71 @@
+package com.es.phoneshop.dao.impl;
+
+import com.es.phoneshop.dao.ProductDao;
+import com.es.phoneshop.dao.exception.ProductNotFoundException;
+import com.es.phoneshop.model.product.Product;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Currency;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ArrayListProductDao implements ProductDao {
+    private long maxId;
+    private List<Product> products;
+
+    public ArrayListProductDao() {
+        products = new ArrayList<>();
+        saveSampleProducts();
+    }
+
+    @Override
+    public synchronized Product getProduct(Long id) throws ProductNotFoundException {
+        return products.stream()
+                .filter(product -> id.equals(product.getId()))
+                .findAny()
+                .orElseThrow(() -> new ProductNotFoundException());
+    }
+
+    @Override
+    public synchronized List<Product> findProducts() {
+        return products.stream()
+                .filter(product -> product.getPrice() != null)
+                .filter(product -> product.getStock() > 0)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public synchronized void save(Product product) {
+        if (products.contains(product)) {
+            try {
+                Product productFromList = getProduct(product.getId());
+                delete(productFromList.getId());
+                productFromList = product;
+                products.add(productFromList);
+                return;
+            } catch (ProductNotFoundException e) {
+                throw new Error("Undefined error" + e);
+            }
+        }
+        product.setId(maxId++);
+        products.add(product);
+    }
+
+    @Override
+    public synchronized void delete(Long id) throws ProductNotFoundException {
+        products.remove(getProduct(id));
+    }
+
+    private void saveSampleProducts() {
+        Currency usd = Currency.getInstance("USD");
+        save(new Product("sgs", "Samsung Galaxy S", new BigDecimal(100), usd, 100, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg"));
+        save(new Product("sgs2", "Samsung Galaxy S II", new BigDecimal(200), usd, 0, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S%20II.jpg"));
+        save(new Product("sgs3", "Samsung Galaxy S III", new BigDecimal(300), usd, 5, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S%20III.jpg"));
+        save(new Product("iphone", "Apple iPhone", new BigDecimal(200), usd, 10, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Apple/Apple%20iPhone.jpg"));
+        save(new Product("iphone6", "Apple iPhone 6", new BigDecimal(1000), usd, 30, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Apple/Apple%20iPhone%206.jpg"));
+        save(new Product("htces4g", "HTC EVO Shift 4G", new BigDecimal(320), usd, 3, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/HTC/HTC%20EVO%20Shift%204G.jpg"));
+        save(new Product("sec901", "Sony Ericsson C901", new BigDecimal(420), usd, 30, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Sony/Sony%20Ericsson%20C901.jpg"));
+        save(new Product("xperiaxz", "Sony Xperia XZ", new BigDecimal(120), usd, 100, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Sony/Sony%20Xperia%20XZ.jpg"));
+    }
+}
--- src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
@@ -1,9 +1,17 @@
 package com.es.phoneshop.model.product;
 
+import com.es.phoneshop.dao.ProductDao;
+import com.es.phoneshop.dao.exception.ProductNotFoundException;
+import com.es.phoneshop.dao.impl.ArrayListProductDao;
+import org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import java.math.BigDecimal;
+import java.util.Currency;
+
+import static org.junit.Assert.*;
+
 
 public class ArrayListProductDaoTest
 {
@@ -16,6 +24,17 @@ public class ArrayListProductDaoTest
 
     @Test
     public void testFindProductsNoResults() {
-        assertTrue(productDao.findProducts().isEmpty());
+        assertFalse(productDao.findProducts().isEmpty());
+    }
+
+    @Test
+    public void testSaveNewProduct() throws ProductNotFoundException {
+        Currency usd = Currency.getInstance("USD");
+        Product product = new Product("test-prod", "Samsung Galaxy SA", new BigDecimal(100), usd, 100, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg");
+        productDao.save(product);
+        assertTrue(product.getId()>0);
+        Product result = productDao.getProduct(product.getId());
+        assertNotNull(result);
+        assertEquals("test-prod", result.getCode());
     }
 }
