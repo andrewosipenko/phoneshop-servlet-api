052f28f4f43f14349cbe06189410adb155dced48
MadPashkaZhuk <paskam1note@gmail.com>
2023-01-02 21:23:25 +0300

    Tasks: 1-6

--- src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
@@ -3,7 +3,10 @@ package com.es.phoneshop.model.product;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import java.math.BigDecimal;
+import java.util.Currency;
+
+import static org.junit.Assert.*;
 
 public class ArrayListProductDaoTest
 {
@@ -16,6 +19,36 @@ public class ArrayListProductDaoTest
 
     @Test
     public void testFindProductsNoResults() {
-        assertTrue(productDao.findProducts().isEmpty());
+        assertFalse(productDao.findProducts().isEmpty());
+    }
+
+    @Test
+    public void testFindProductsWithCondition() {
+        assertNotEquals(productDao.getAmountOfProducts(), productDao.findProducts().size());
+        assertEquals(productDao.getAmountOfProducts(), productDao.findProducts().size() + 1);
+    }
+    @Test
+    public void testSaveNewProduct() throws ProductNotFoundException {
+        Currency usd = Currency.getInstance("USD");
+        Product product = new Product("test-product", "Samsung Galaxy S", new BigDecimal(100), usd, 100, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg");
+        productDao.save(product);
+        assertTrue(product.getId() > 0);
+        Product result = productDao.getProduct(product.getId());
+        assertNotNull(result);
+        assertEquals("test-product", result.getCode());
+
+    }
+    @Test(expected = ProductNotFoundException.class)
+    public void testSaveAlreadyExistedProduct() throws ProductNotFoundException {
+        Currency usd = Currency.getInstance("USD");
+        Product product = new Product(4L,"sgs", "Samsung Galaxy S", new BigDecimal(100), usd, 100, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg");
+        productDao.save(product);
+        Product result2 = productDao.getProduct(4L);
+    }
+
+    @Test(expected = ProductNotFoundException.class)
+    public void testDeleteProduct() throws ProductNotFoundException {
+        productDao.delete(5L);
+        productDao.getProduct(5L);
     }
 }
