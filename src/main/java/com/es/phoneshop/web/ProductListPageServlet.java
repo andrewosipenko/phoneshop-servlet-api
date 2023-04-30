bba1b6560cb902fb98e700533eb12529d6812154
Maxim-Kulikov <kulikov.m.a102@gmail.com>
2023-04-30 13:20:23 +0300

    completed tasks in the lecture 1

--- src/main/java/com/es/phoneshop/web/ProductListPageServlet.java
+++ src/main/java/com/es/phoneshop/web/ProductListPageServlet.java
@@ -1,7 +1,10 @@
 package com.es.phoneshop.web;
 
+import com.es.phoneshop.model.product.ArrayListProductDao;
 import com.es.phoneshop.model.product.Product;
 
+import com.es.phoneshop.model.product.ProductDao;
+import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,9 +16,17 @@ import java.util.Currency;
 import java.util.List;
 
 public class ProductListPageServlet extends HttpServlet {
+    private ProductDao productDao;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        productDao = new ArrayListProductDao();
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setAttribute("products", getSampleProducts());
+        request.setAttribute("products", productDao.findProducts());
         request.getRequestDispatcher("/WEB-INF/pages/productList.jsp").forward(request, response);
     }
 
--- src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
@@ -2,20 +2,57 @@ package com.es.phoneshop.model.product;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 
-import static org.junit.Assert.assertTrue;
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 public class ArrayListProductDaoTest
 {
     private ProductDao productDao;
+    private Product productToSave;
 
     @Before
     public void setup() {
         productDao = new ArrayListProductDao();
+        Currency usd = Currency.getInstance("USD");
+        productToSave = new Product("test", "test", new BigDecimal(150), usd, 40, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Siemens/Siemens%20SXG75.jpg");
     }
 
     @Test
-    public void testFindProductsNoResults() {
-        assertTrue(productDao.findProducts().isEmpty());
+    public void atTheBeginningArrayIsNotEmpty(){
+        assertNotNull(productDao.findProducts());
+        assertFalse(productDao.findProducts().isEmpty());
+    }
+
+    @Test
+    public void getProduct(){
+        Long id = (long) (productDao.findProducts().size() - 1);
+        assertNotNull(productDao.getProduct(id));
+    }
+
+    @Test
+    public void findProducts(){
+        for(Product product: productDao.findProducts()){
+            assertNotNull(product);
+            assertNotNull(product.getPrice());
+            assertTrue(product.getStock()>0);
+        }
+    }
+
+    @Test
+    public void save(){
+        productDao.save(productToSave);
+        Product result = productDao.getProduct(productToSave.getId());
+        assertEquals(result, productToSave);
+    }
+
+    @Test
+    public void delete(){
+        productDao.delete(1L);
+        assertThrows(RuntimeException.class,()-> productDao.getProduct(1L));
     }
 }
